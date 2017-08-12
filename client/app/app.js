671e1bec00ef2e227f22cd06348a6658dddb886e
Adsi 1261718 <adsi1261718@rootbean>
2017-08-12 07:50:19 -0500

    se creo un nuevo proyecto

---
+++ client/app/app.js
@@ -0,0 +1,23 @@
+'use strict';
+
+angular.module('videoClubApp', [
+	'videoClubApp.constants',
+	 'ngCookies',
+	 'ngResource',
+	 'ngSanitize',
+    'ui.router',
+    'ui.bootstrap',
+		'satellizer'
+  ])
+.constant("API","http://localhost:8080/adsi2017_backend_completo-master")
+.config(function(API, $authProvider){
+	$authProvider.loginURL = API + '/api/auth/login';
+	$authProvider.tokenName = 'token';
+	$authProvider.tokenPrefix = 'startUpApp';
+})
+
+  .config(function($urlRouterProvider, $locationProvider) {
+    $urlRouterProvider.otherwise('/');
+
+    $locationProvider.html5Mode(true);
+  });
